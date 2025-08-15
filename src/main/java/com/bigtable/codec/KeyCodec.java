6a7ac6603eda538f78317403beb2e38242f93488
VinhDuyLe <leduyvinh91@gmail.com>
2025-08-14 22:55:15 -0700

    Phase A add KeyCodec

--- src/main/java/com/bigtable/codec/KeyCodec.java
+++ src/main/java/com/bigtable/codec/KeyCodec.java
@@ -1,4 +1,33 @@
 package com.bigtable.codec;
 
-public class KeyCodec {
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+/**
+ * Encodes composite keys: [rowLen][row][familyId][qualLen][qualifier][timestampDesc(8)]
+ */
+public final class KeyCodec {
+    private KeyCodec() {
+    }
+
+    public static byte[] encodeCompositeKey(byte[] rowKey, int familyId, byte[] qualifier, long timestampMicros) {
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            Varint.writeUnsignedVarInt(out, rowKey.length);
+            out.write(rowKey);
+            Varint.writeUnsignedVarInt(out, familyId);
+            Varint.writeUnsignedVarInt(out, qualifier.length);
+            out.write(qualifier);
+            long desc = (~timestampMicros) & 0xFFFFFFFFFFFFFFFFL; // UINT64_MAX - ts
+            ByteBuffer bb = ByteBuffer.allocate(8).order(ByteOrder.BIG_ENDIAN);
+            bb.putLong(desc);
+            out.write(bb.array());
+            return out.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/bigtable/codec/ZstdCompression.java
+++ src/main/java/com/bigtable/codec/ZstdCompression.java
@@ -1,4 +1,19 @@
 package com.bigtable.codec;
 
-public class ZstdCompression {
+import com.github.luben.zstd.Zstd;
+
+public final class ZstdCompression {
+    private ZstdCompression() {
+    }
+
+    public static byte[] compress(byte[] src, int lvl) {
+        return Zstd.compress(src, lvl);
+    }
+
+    public static byte[] decompress(byte[] compressed, int orginalSize) {
+        byte[] dest = new byte[orginalSize];
+        long r = Zstd.decompress(dest, compressed);
+        if (r <= 0) throw new RuntimeException("zstd decompress error: " + r);
+        return dest;
+    }
 }
